c4c22e0f009e5ea2045b3444c5287bd19f7691e8
webber12 <web-ber12@yandex.ru>
2015-03-11 21:20:04 +0200

    small fix - переименован css-класс для категории фильтров

--- assets/snippets/eFilter/eFilter.class.php
+++ assets/snippets/eFilter/eFilter.class.php
@@ -150,7 +150,7 @@ public function renderFilterBlock ($filter_cats, $filter_values_full, $filter_va
     $output = '';
     $fc = 0;
     foreach ($filter_cats as $cat_name => $tmp) {
-        $output .= '<div class="eFilter_cat eFilter_cat' . $fc . '">';
+        $output .= '<div class="eFiltr_cat eFiltr_cat' . $fc . '">';
         if (count($filter_cats) > 1) {$output .= $this->parseTpl(array('[+cat_name+]'), array($cat_name), $filterCatName);}
         $tv_elements = $this->getDefaultTVValues($tmp);
         foreach ($tmp as $tv_id => $tmp2) {
