dbdce7a4676fc7489b0519e3ce34f760b0602870
webber12 <web-ber12@yandex.ru>
2018-03-02 18:05:34 +0200

    cancel

--- assets/snippets/eFilter/snippet.eFilter.php
+++ assets/snippets/eFilter/snippet.eFilter.php
@@ -70,10 +70,6 @@ if (!empty($eFltr->list_tv_ids)) {
 //так и с использованием фильтра
 //на выходе получаем список id подходящих ресурсов через запятую
 $DLparams = array('parents' => $eFltr->docid, 'depth' => '3', 'addWhereList' => ((isset($eFltr->params['addWhereList']) && !empty($eFltr->params['addWhereList'])) ? $eFltr->params['addWhereList'] . ' AND ' : '') . 'c.template IN(' . $eFltr->params['product_templates_id'] . ')', 'makeUrl' => '0');
-$filter_ids = $modx->getPlaceholder("eFilter_filter_ids");
-if ($filter_ids && $filter_ids != '') {
-    $DLparams['addWhereList'] .= ' AND c.id IN (' . $filter_ids . ') ';
-}
 $DLparamsAPI = array('JSONformat' => 'new', 'api' => 'id', 'selectFields' => 'c.id');
 $allProducts = $eFltr->getCategoryAllProducts($eFltr->docid, $eFltr->tv_category_tag);
 if (!empty($allProducts) && $allProducts != '') {
